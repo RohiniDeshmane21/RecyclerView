1d781154b025deac22f7160d843761b83884efc6
RohiniDeshmane21 <rohinideshmane.21@gmail.com>
2019-09-06 10:53:53 +0530

    After some updates

--- app/src/main/java/com/example/rohini/recyclerview/MainActivity.java
+++ app/src/main/java/com/example/rohini/recyclerview/MainActivity.java
@@ -52,6 +52,7 @@ public class MainActivity extends AppCompatActivity {
                 }
 
             });
+
             @Override
             public boolean onInterceptTouchEvent(RecyclerView Recyclerview, MotionEvent motionEvent) {
 
